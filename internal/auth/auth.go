bad1652db60c3ec62e30e6913ddb30d1aaee6f2b
Kirill Nepomnyashy <fulikozzz@gmail.com>
2026-05-25 18:32:37 +0700

    БД введена в проект
    - в auth.go вместо структуры пользователя используется БД
    - добавлено хранилище SQLite для пользователей и сообщений
    - создана структура таблиц
    - перенесено хранение данных пользователей из ОЗУ (map) в БД
    - обновлен `auth.Manager` для использования БД
    - добавлена функция получения истории сообщений между двумя пользователями

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -2,68 +2,45 @@ package auth
 
 import (
     "errors"    // работа с ошибками
-    "sync"      // синхронизация доступа
+
+    "github.com/fulikozzz/AuroraMsgr/internal/storage"
 )
 
-type User struct {
-    Username string
-    Password string
-}
 
 type Manager struct {
-    mu sync.RWMutex
-    users map[string]User
+    storage *storage.Storage
 }
 
 // Создание нового менеджера аутентификации
-func NewManager() *Manager {
+func NewManager(s *storage.Storage) *Manager {
     return &Manager{
-        users: make(map[string]User),
+        storage: s,
     }
 }
 
 // Регистрация нового пользователя
 func (m *Manager) Register(username, password string) error {
-    m.mu.Lock() // блокируем для записи 
-    defer m.mu.Unlock() // разблокируем после записи
-
     if username == "" || password == "" {
         return errors.New("имя пользователя и пароль не могут быть пустыми")
     }
 
-    if _, exists := m.users[username]; exists {
-        return errors.New("пользователь уже существует")
-    }
-
-    m.users[username] = User{
-        Username: username,
-        Password: password,
+    if err := m.storage.CreateUser(username, password); err != nil {
+        return err
     }
 
     return nil
 }
 
 func (m *Manager) Login (username, password string) error {
-    m.mu.RLock() // блокируем для чтения
-    defer m.mu.RUnlock() // разблокируем после чтения
-    
-    user, exists := m.users[username]
-   
-    if !exists {
+    _, storedPassword, err := m.storage.GetUser(username)
+
+    if err != nil {
         return errors.New("пользователь не найден")
     }
 
-    if user.Password != password {
+    if storedPassword != password {
         return errors.New("неверный пароль")
     }
     
     return nil
-}
-
-func (m *Manager) Exists(username string) bool {
-    m.mu.RLock()
-    defer m.mu.RUnlock()
-
-    _, exists := m.users[username]
-    return exists
 }
