f80ab013200c31e55dd69d71eb7cc4584390b4e3
Kirill Nepomnyashy <fulikozzz@gmail.com>
2026-05-26 15:15:49 +0700

    Реализована функция отправки сообщения конкретному пользователю

--- internal/chat/chat.go
+++ internal/chat/chat.go
@@ -26,3 +26,39 @@ func NewHub() *Hub {
     }
 }
 
+// Регистрирует клиента в Hub
+func (h *Hub) Register(username string, conn net.Conn) {
+    h.mu.Lock()
+    defer h.mu.Unlock()
+
+    h.clients[username] = &Client{
+        Username: username,
+        Conn: conn,
+    }
+}
+
+// Удаляет клиента из Hub
+func (h *Hub) Unregister(username string){
+    h.mu.Lock()
+    defer h.mu.Unlock()
+
+    delete(h.clients, username)
+}
+
+// Отправка сообщения пользовател.ю
+func (h *Hub) Send(from, to, payload string) error {
+	h.mu.RLock()
+	defer h.mu.RUnlock()
+
+	client, exists := h.clients[to]
+	if !exists {
+		return fmt.Errorf("пользователь '%s' не в сети", to)
+	}
+
+	return protocol.Send(client.Conn, protocol.Packet{
+		Type:    protocol.PacketMessage,
+		From:    from,
+		To:      to,
+		Payload: payload,
+	})
+}
